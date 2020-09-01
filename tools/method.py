dcae39eb2c43922a82eae55a9e3752f6a05356a3
dovelx <dovelx@163.com>
2020-09-01 09:13:59 +0800

    sec commit

---
+++ tools/method.py
@@ -0,0 +1,171 @@
+import requests
+import json
+
+def gy(caseinfo,headers,cookies):
+    rs = requests.get(url=caseinfo['url'], headers=headers, cookies=cookies)
+    #请求接口
+    #rs=requests.get(url, headers = headers,cookies=cookies)
+    if rs.status_code == 200:
+        #返回值转码name
+        data = rs.content.decode('utf-8')
+        #json格式化
+        data = json.loads(data)
+        #获取接口返回状态
+        status= data['status']
+        if status == 3200:
+            caseinfo['result'] = "pass"
+            print("获取预约列表成功", status)
+            #获取本次作业预约的work_appoint_id
+            data = data['data']['voset']['voList']
+            #print(data)
+            temp = []
+            for a in data:
+                #print("\n\n",a)
+                if a['workname']  == caseinfo['data']:
+                    temp.append(a['work_appoint_id'])
+            work_appoint_id = temp[0]
+            #当前最大work_appoint_id加1
+            #work_appoint_id =work_appoint_id+1
+            print("work_appoint_idx",work_appoint_id)
+            return caseinfo['result'],work_appoint_id
+        else:
+            caseinfo['result'] = "fail"
+            return 0
+
+def py(caseinfo,headers,cookies):
+    rs = requests.post(url=caseinfo['url'], json=caseinfo['data'], headers=headers, cookies=cookies)
+    if rs.status_code == 200:
+        # 返回值转码
+        data = rs.content.decode('utf-8')
+        # json化
+        data = json.loads(data)
+        # 获取接口返回状态
+        sta = data['status']
+        if sta == 3200:
+            #print("作业预约成功", sta)
+            caseinfo['result'] = "pass"
+            return caseinfo['result'],1
+        else:
+            caseinfo['result'] = "fail"
+            return data,1
+    else:
+        caseinfo['result'] = "fail"
+        return rs.status_code
+
+
+def g(caseinfo,headers,cookies):
+    rs = requests.get(url=caseinfo['url'], headers=headers, cookies=cookies)
+    # 返回值转码
+    data = rs.content.decode('utf-8')
+    # json化
+    data = json.loads(data)
+    # 获取接口返回状态
+    sta = data['status']
+    if sta == 3200:
+        #print("作业预约成功", sta)
+        caseinfo['result'] = "pass"
+        return caseinfo['result']
+    else:
+        caseinfo['result'] = "fail"
+        return data
+
+def gh(caseinfo,headers,cookies):
+    rs = requests.get(url=caseinfo['url'], headers=headers, cookies=cookies)
+    # 返回值转码
+    #data = rs.content.decode('utf-8')
+    # json化
+    #data = json.loads(data)
+    # 获取接口返回状态
+    #sta = data['status']
+    sta = rs.status_code
+    if sta == 200:
+        #print("作业预约成功", sta)
+        caseinfo['result'] = "pass"
+        return caseinfo['result']
+    else:
+        caseinfo['result'] = "fail"
+        return sta
+def pa(caseinfo,headers,cookies):
+    rs = requests.post(url=caseinfo['url'], json=caseinfo['data'], headers=headers, cookies=cookies)
+    if rs.status_code == 200:
+        # 返回值转码
+        data = rs.content.decode('utf-8')
+        # json化
+        data = json.loads(data)
+        # 获取接口返回状态
+        sta = data['status']
+        if sta == 3200:
+            #print("作业预约成功", sta)
+            caseinfo['result'] = "pass"
+            return caseinfo['result']
+        else:
+            caseinfo['result'] = "fail"
+            return data
+    else:
+        caseinfo['result'] = "fail"
+        return rs.status_code
+
+
+def pm(caseinfo,mheaders):
+
+    rs = requests.post(url = caseinfo['url'],json=caseinfo['data'],headers=mheaders)
+    if rs.status_code == 200:
+        # 返回值转码
+        data = rs.content.decode('utf-8')
+        #json格式化
+        data = json.loads(data)
+        # 获取接口返回状态
+        sta = data['status']
+        if sta == 3200:
+            # print("作业预约成功", sta)
+            caseinfo['result'] = "pass"
+            return caseinfo['result']
+        else:
+            caseinfo['result'] = "fail"
+            return data
+    else:
+        caseinfo['result'] = "fail"
+        return rs.status_code
+
+def gm(caseinfo,headers):
+    rs = requests.get(url=caseinfo['url'], headers=headers)
+    #print("debug",rs.content)
+    # 返回值转码
+    #data = rs.content.decode('utf-8')
+    # json化
+    #data = json.loads(data)
+    # 获取接口返回状态
+    #print("data",data)
+    sta = rs.status_code
+    #if caseinfo['id'] == 100:
+    #    insert = data['data']['insert__']
+    if sta == 200:
+        #print("作业预约成功", sta)
+        caseinfo['result'] = "pass"
+        return caseinfo['result']
+
+    else:
+        caseinfo['result'] = "fail"
+        return sta
+
+def gj(caseinfo,headers):
+    rs = requests.get(url=caseinfo['url'], headers=headers)
+    #print("debug",rs.content)
+    # 返回值转码
+    data = rs.content.decode('utf-8')
+    # json化
+    data = json.loads(data)
+    # 获取接口返回状态
+    #print("data",data)
+    sta = data['status']
+
+    #if caseinfo['id'] == 100:
+    #    insert = data['data']['insert__']
+    if sta == 3200:
+        #print("作业预约成功", sta)
+        caseinfo['result'] = "pass"
+        return caseinfo['result']
+
+    else:
+        caseinfo['result'] = "fail"
+        return data
---
+++ tools/sqls.py
@@ -0,0 +1,16 @@
+#pc
+#操作hse_work_ticket表，获取workticketid获取
+#ticket = 'select workticketid from hse_work_ticket order by workticketid desc limit 1'
+#SELECT workticketid FROM `hap_hse_cqsh`.`hse_work_ticket` WHERE worktype = 'shex' ORDER BY `created_dt` DESC LIMIT 1
+ticket = "SELECT workticketid FROM `hap_hse_cqsh`.`hse_work_ticket` WHERE worktype = 'shex' ORDER BY `created_dt` DESC LIMIT 1"
+ticket_1 = "SELECT workticketid FROM `hap_hse_cqsh`.`hse_work_ticket` WHERE worktype = 'xkz' ORDER BY `created_dt` DESC LIMIT 1"
+ts = 'select ts from hse_work_ticket order by ts desc limit 1'
+#连续号-作业任务添加ID
+worktaskid1 = 'select worktaskid from hse_work_ticket ORDER BY created_dt desc limit 1'
+#断号
+worktaskid = 'select worktaskid from hse_work_task ORDER BY created_dt desc limit 1;'
+#worktaskid = 'select worktaskid from hse_work_ticket ORDER BY workticketid desc limit 1;'
+appoint_id ='SELECT worktaskid from hse_safety_task ORDER BY  worktaskid desc LIMIT 1;'
+sql_query_work_jsaid='SELECT jsaid from hse_safety_analysis ORDER BY  jsaid desc LIMIT 1'
+sql_query_work_safeclarid='SELECT safeclarid from hse_safety_disclosure ORDER BY  safeclarid desc LIMIT 1'
+sql_query_work_appointid ='SELECT work_appoint_id from hse_work_appoint ORDER BY  created_dt desc LIMIT 1'
